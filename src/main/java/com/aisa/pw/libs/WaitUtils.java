ed5dbf04d41dc4bf761f079c259273770cee7227
Ashok Maurya <ashok.samar1@gmail.com>
2025-03-01 14:59:44 +0530

    ***** Added wait *****

--- src/main/java/com/aisa/pw/libs/WaitUtils.java
+++ src/main/java/com/aisa/pw/libs/WaitUtils.java
@@ -7,4 +7,23 @@ public class WaitUtils {
     public static void waitForElement(Page page, String selector) {
         page.locator(selector).waitFor();
     }
+
+    public static void waitForText(Page page, String selector, String text) {
+        page.waitForCondition(() -> page.locator(selector).textContent().contains(text),
+                new Page.WaitForConditionOptions().setTimeout(5000));
+    }
+
+    public static void waitForURL(Page page, String expectedURL) {
+        page.waitForCondition(() -> page.url().equals(expectedURL),
+                new Page.WaitForConditionOptions().setTimeout(5000));
+    }
+
+    public static void waitForAttribute(Page page, String selector, String attribute, String expectedValue) {
+        page.waitForCondition(() -> page.locator(selector).getAttribute(attribute).equals(expectedValue),
+                new Page.WaitForConditionOptions().setTimeout(5000));
+    }
+
+    public static void waitForTimeout(Page page, int milliseconds) {
+        page.waitForTimeout(milliseconds);
+    }
 }
